bb3a2880e0050dc60599c18bcb738383654bb3f2
gaba23 <gabrielalvesacardoso@gmail.com>
2025-05-11 23:49:49 -0300

    adicionando cors para conexao com front end

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { AppDataSource } from './database/data-source';
 import authRoutes from './routes/authRoutes';
 import livroRoutes from './routes/livroRoutes';
 
 dotenv.config();
 const app = express();
+
+// Configuração do CORS
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/livros', livroRoutes);
